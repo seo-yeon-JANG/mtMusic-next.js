3ffcdcdfa64aa0f46d0fe9c08e4b3d4dcd2d30fe
seoyeon <tjsus247@naver.com>
2024-04-18 10:19:31 +0900

    app componetns add, folder add

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import { sleep } from "@/lib/utils";
+import React from "react";
+
+const page = async () => {
+  // await sleep(2000);
+  // 서버 사이드 로딩 중 문제 발생 시 errorpage 노출
+  throw new Error("my error");
+  // return <div>HomePage</div>;
+};
+
+export default page;
